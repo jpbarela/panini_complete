b84136fbda4d2dbc17155b7520348fcdbfbaf394
larispardo <luisferlaris@gmail.com>
2022-09-12 20:48:28 -0500

    ADD new class for obtaining the results of stickers after expenditure x

--- Panini/StickerCollection.py
+++ Panini/StickerCollection.py
@@ -1,10 +1,11 @@
 from Panini import Pack
 
+
 class StickerCollection:
 
     def __init__(self, size, stickers_requestable, pack_size, already_owned):
         self.__size = size
-        self.collection = [0] * (size-already_owned) + [1] * already_owned
+        self.collection = [0] * (size - already_owned) + [1] * already_owned
         self.stickers = already_owned
         self.__threshold = size - stickers_requestable
         self.__pack_size = pack_size
@@ -23,6 +24,34 @@ class StickerCollection:
             if self.collection[i] == 1:
                 self.stickers += 1
 
+
+class CollectionOnBudget:
+
+    def __init__(self, size, pack_size, already_owned, money_to_spend, pack_price):
+        self.__size = size
+        self.collection = [0] * (size - already_owned) + [1] * already_owned
+        self.stickers = already_owned
+        self.spent = 0
+        self.price = pack_price
+        self.__threshold = money_to_spend
+        self.__pack_size = pack_size
+
+    def packs_opened(self):
+        packs = 0
+        while self.spent < self.__threshold and self.stickers < self.__size:
+            self.spent += self.price
+            self.add_pack()
+            packs += 1
+        return self.stickers
+
+    def add_pack(self):
+        new_pack = Pack.Pack(self.__pack_size, self.__size)
+        for i in new_pack.contents():
+            self.collection[i] += 1
+            if self.collection[i] == 1:
+                self.stickers += 1
+
+
 class SpecificSticker:
 
     def __init__(self, size, pack_size):
@@ -32,7 +61,7 @@ class SpecificSticker:
         # this is simulating the probability of getting card #10 given that the album is empty. i.e. the probability of obtaining a given card.
         self.__sticker = 10
         self.__pack_size = pack_size
-        self.found=False
+        self.found = False
 
     def packs_till_found(self):
         packs = 0
--- simulate_packs.py
+++ simulate_packs.py
@@ -8,33 +8,50 @@ from Accumulator import Accumulator
 
 parser = argparse.ArgumentParser('Simulate creating a Panini sticker collection')
 
-parser.add_argument('runs', metavar='N', type= int)
+parser.add_argument('runs', metavar='N', type=int)
 
 runs = parser.parse_args().runs
 
 results = Accumulator.Accumulator()
 resultsSticker = Accumulator.Accumulator()
+resultsSpend = Accumulator.Accumulator()
 
 for i in range(runs):
-    collection = StickerCollection.StickerCollection(Panini.NUMBER_OF_STICKERS, Panini.STICKERS_TO_REQUEST, Panini.STICKERS_PER_PACK, Panini.ALREADY_OWNED_PACKS)
+    collection = StickerCollection.StickerCollection(Panini.NUMBER_OF_STICKERS, Panini.STICKERS_TO_REQUEST,
+                                                     Panini.STICKERS_PER_PACK, Panini.ALREADY_OWNED_PACKS)
     forSticker = StickerCollection.SpecificSticker(Panini.NUMBER_OF_STICKERS, Panini.STICKERS_PER_PACK)
+    fix_spenses = StickerCollection.CollectionOnBudget(Panini.NUMBER_OF_STICKERS, Panini.STICKERS_PER_PACK,
+                                                       Panini.ALREADY_OWNED_PACKS,Panini.TO_SPEND, Panini.PACK_PRICE)
     results.add_value_packs(collection.packs_till_complete())
+    resultsSpend.add_value_packs(fix_spenses.packs_opened())
     sticker_data = forSticker.packs_till_found()
     resultsSticker.add_value_packs(sticker_data[0])
     resultsSticker.add_value_stickers(sticker_data[1])
 
-
 print("Number of runs: {0}".format(runs))
-print("promedio numero de empaques para llenar el album: {0} (por un total de  {1})".format(math.ceil(results.average_packs()), math.ceil(results.average_packs()) * 18))
-print("minimo numero de empaques para llenar el album: {0} (por un total de  {1})".format(results.min_packs(), results.min_packs() * 18))
-print("maximo numero de empaques para llenar el album: {0} (por un total de  {1})".format(results.max_packs(), results.max_packs() * 18))
+print("promedio numero de empaques para llenar el album: {0} (por un total de  {1})".format(
+    math.ceil(results.average_packs()), math.ceil(results.average_packs()) * Panini.PACK_PRICE))
+print("minimo numero de empaques para llenar el album: {0} (por un total de  {1})".format(results.min_packs(),
+                                                                                          results.min_packs() * Panini.PACK_PRICE))
+print("maximo numero de empaques para llenar el album: {0} (por un total de  {1})".format(results.max_packs(),
+                                                                                          results.max_packs() * Panini.PACK_PRICE))
 print("desviacion estandar de empaques para llenar el album: {0}".format(results.standard_deviation_packs()))
 print()
-print("promedio numero de empaques para obtener un sticker: {0} (por un total de  {1})".format(math.ceil(resultsSticker.average_packs()), math.ceil(resultsSticker.average_packs()) * 18))
-print("minimo numero de empaques para obtener un sticker: {0} (por un total de  {1})".format(resultsSticker.min_packs(), resultsSticker.min_packs() * 18))
-print("maximo numero de empaques para obtener un sticker: {0} (por un total de  {1})".format(resultsSticker.max_packs(), resultsSticker.max_packs() * 18))
+print("promedio numero de empaques para obtener un sticker: {0} (por un total de  {1})".format(
+    math.ceil(resultsSticker.average_packs()), math.ceil(resultsSticker.average_packs()) * Panini.PACK_PRICE))
+print("minimo numero de empaques para obtener un sticker: {0} (por un total de  {1})".format(resultsSticker.min_packs(),
+                                                                                             resultsSticker.min_packs() * Panini.PACK_PRICE))
+print("maximo numero de empaques para obtener un sticker: {0} (por un total de  {1})".format(resultsSticker.max_packs(),
+                                                                                             resultsSticker.max_packs() * Panini.PACK_PRICE))
 print("desviacion estandar de empaques para obtener un sticker: {0}".format(resultsSticker.standard_deviation_packs()))
 print()
 print("promedio de estampa que fue el sticker: {0}".format(math.ceil(resultsSticker.average_stickers())))
 print("minimo numero de estampa que fue el sticker: {0}".format(resultsSticker.min_stickers()))
 print("maximo numero de estampa que fue el sticker: {0}".format(resultsSticker.max_stickers()))
+print()
+print("promedio numero de empaques para obtener un sticker: {0}".format(
+    math.ceil(resultsSpend.average_packs())))
+print("minimo numero de empaques para obtener un sticker: {0} ".format(resultsSpend.min_packs()))
+print("maximo numero de empaques para obtener un sticker: {0}".format(resultsSpend.max_packs()))
+print("desviacion estandar de empaques para obtener un sticker: {0}".format(resultsSpend.standard_deviation_packs()))
+print()
